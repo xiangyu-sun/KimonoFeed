72ae05136e40fa3e75bffd3c75969c38476fa05f
Xiangyu Sun <xiangyu.sun@dnata.com>
2018-12-22 20:08:56 +0400

    Initial Commit

---
+++ KimonoFeedTests/KimonoFeedTests.swift
@@ -0,0 +1,34 @@
+//
+//  KimonoFeedTests.swift
+//  KimonoFeedTests
+//
+//  Created by 孙翔宇 on 12/22/18.
+//  Copyright © 2018 孙翔宇. All rights reserved.
+//
+
+import XCTest
+@testable import KimonoFeed
+
+class KimonoFeedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
